59336081c16f25b3c5b4b294dca752e7c79291c2
inflatus <inflatus@gmail.com>
2017-02-07 12:47:23 +0000

    Major change to xkcd with help from Joe K

--- downloadXkcd.py
+++ downloadXkcd.py
@@ -1,47 +1,103 @@
 #!/usr/bin/env python3
-#downloadXkcd.py - Downloads every single XKCD comic
+# downloadXkcd.py - Downloads every single XKCD comic
 
-import requests
 import os
+
 import bs4
+import requests
 
-url = 'http://xkcd.com'                  #starting url
-os.makedirs('xkcd', exist_ok = True)     #store compis in ./xkcd
 
-while not url.endswith('#'):
-    #TODO: Download the page
-    print('Downloading page %s...' % url)
-    res = requests.get(url)
-    res.raise_for_status()
-
-    soup = bs4.BeautifulSoup(res.text)
-
-    #TODO: Find the url of the comic image
-    comicElem = soup.select('#comic img')
-    if comicElem == []:
-        print('Could not find comic image.')
+def _is_image_valid(image_url):
+    if "imgs.xkcd.com/comics" in image_url:
+        return True
     else:
-        try:
-            comicUrl = 'http:' + comicElem[0].get('src')
+        return False
 
-    #TODO: Download the image
-        print('Downloading image %s...' % (comicUrl))
-        res = requests.get(comicUrl)
-        res.raise_for_status()
 
-    #TODO: Skip this comic
-        prevLink = soup.select('a[rel="prev"]')[0]
-        url = 'http://xkcd.com' + prevLink.get ('href')
-        continue
+def get_image(soup):
+    return 'http:' + soup.select('#comic img')[0].get('src')
 
-    #TODO: Save the image to ./xkcd
-        imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
-        for chunk in res.iter_content(100000):
-            imageFile.write(chunk)
-        imageFile.close()
 
-    #TODO: Get the previous url
-        prevLink = soup.select('a[rel="prev"]')[0]
-        url = 'http://xkcd.com' + prevLink.get('href')
+def image_is_there(page):
+    comicElem = page.select('#comic img')
+    if comicElem == []:
+        return False
 
-print('done.')
+    image_link = get_image(page)
+
+    if _is_image_valid(image_link):
+        return True
+
+    return False
+
+
+def download_image(url):
+    print('Downloading image %s...' % (url))
+    response = requests.get(url)
+    response.raise_for_status()
+    return response
+
+
+def get_prev_url(page, number_only=False):
+    prevLink = page.select('a[rel="prev"]')[0]
+
+    if '#' in prevLink:  # comic #1 has a # as the prev identifier
+        prevLink = '/#'
+    new_url = 'http://xkcd.com' + prevLink.get('href')
+
+    if number_only is True:
+        cleaned_number = prevLink.get('href').strip("/")
+        if "#" in cleaned_number:  # checking for comic #1 again
+            return 1
+        else:
+            return int(cleaned_number) + 1
+
+    return new_url
+
+
+def save_image(c_url, image, page):
+    filename = os.path.basename(c_url)
+    filename = str(get_prev_url(page, number_only=True)) + " - " + filename
+    imageFile = open(os.path.join('xkcd', filename), 'wb')
+    for chunk in image.iter_content(100000):
+        imageFile.write(chunk)
+    imageFile.close()
+
+
+def get_new_page(new_url):
+
+    print('Downloading page %s...' % new_url)
+    response = requests.get(new_url)
+    response.raise_for_status()
+
+    return bs4.BeautifulSoup(response.text)
+
+
+def main(url):
+
+    while not url.endswith('#'):
+        soup = get_new_page(url)
+
+        if image_is_there(soup):
+
+            comicUrl = get_image(soup)
+
+            image = download_image(comicUrl)
+
+            # save the image to ./xkcd
+            save_image(comicUrl, image, soup)
+
+            url = get_prev_url(soup)
+        else:
+            print("Can't find image!")
+            # skip this comic
+            url = get_prev_url(soup)
+
+    print('done.')
+
+if __name__ == "__main__":
+    url = 'http://xkcd.com'
+    target_directory = 'xkcd'
+    os.makedirs(target_directory, exist_ok=True)
+
+    main(url)
