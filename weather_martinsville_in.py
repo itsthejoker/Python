ac176179ca937e3e6447ae289d97a47d252f388d
inflatus <inflatus@gmail.com>
2017-02-17 12:45:28 +0000

    cleaned the code a bit

--- weather_martinsville_in.py
+++ weather_martinsville_in.py
@@ -4,12 +4,13 @@ from bs4 import BeautifulSoup
 import requests
 
 #grab the url
-site = requests.get('https://www.wunderground.com/cgi-bin/findweather/getForecast?query=Martinsville%2C+IN')
+the_url = requests.get('https://www.wunderground.com/cgi-bin/findweather/getForecast?query=Martinsville%2C+IN')
 
-soup = BeautifulSoup(site.text, 'html.parser')
+soup = BeautifulSoup(the_url.text, 'html.parser')
 
 #drilling down on data
-site2 = soup.find('div',{'id':'curTemp'})
-site3 = site2.find('span',{'class':'wx-value'})
+url_curtemp = soup.find('div',{'id':'curTemp'})
+wx_value = url_curtemp.find('span',{'class':'wx-value'})
 
-print(site3.text, 'Degrees Fahrenheit')
+print (soup.title.text)
+print(wx_value.text, 'Degrees Fahrenheit')
