d0d96208fe4d09a1f6f66045816635a57421eb18
inflatus <inflatus@gmail.com>
2017-02-24 15:31:30 +0000

    made minor style changes

--- check_ip_sms.py
+++ check_ip_sms.py
@@ -2,7 +2,7 @@ import bs4 as bs
 import urllib.request
 from twilio.rest import TwilioRestClient
 
-# pulling KEY and TOKEN
+# pulling the KEY and TOKEN
 ACCOUNT_KEY = open('twilio_account.txt').read().strip()
 ACCOUNT_TOKEN = open('twilio_token.txt').read().strip()
 
@@ -10,11 +10,11 @@ sauce = urllib.request.urlopen('http://checkip.dyndns.com/').read()
 
 soup = bs.BeautifulSoup(sauce, 'lxml')
 
-# print IP address
+# print the IP address
 print(soup.body.text)
 
 # setting up twilio information
 client = TwilioRestClient(account=(ACCOUNT_KEY), token=(ACCOUNT_TOKEN))
 
-# sending IP address via sms
+# sending the IP address via sms
 client.messages.create(from_="(619) 273-3961", to="(812) 360-9210", body=(soup.body.text))
