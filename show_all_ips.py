24d138525e7018a8c46ceebde42b6f898b6bf30d
inflatus <inflatus@gmail.com>
2017-04-19 22:04:04 +0000

    added host name as well

--- show_all_ips.py
+++ show_all_ips.py
@@ -1,5 +1,8 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
-print(s.getsockname()[0])
+ip_address = (s.getsockname()[0])
+print(ip_address)
+host_name = socket.gethostname()
+print(host_name)
 s.close()
