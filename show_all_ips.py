db46a22957c055ef0aa5f77101373b988f45f783
inflatus <inflatus@gmail.com>
2017-04-19 22:29:49 +0000

    added another IP checker

--- show_all_ips.py
+++ show_all_ips.py
@@ -1,8 +1,16 @@
 import socket
+
+# open the socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
+
+# get IP addresses and host names
 ip_address = (s.getsockname()[0])
-print(ip_address)
 host_name = socket.gethostname()
+
+# print gathered information
+print(ip_address)
 print(host_name)
+
+# close the socket
 s.close()
