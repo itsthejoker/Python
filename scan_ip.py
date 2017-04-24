0d59913c3b3f9d36c646702600df5a225fe3bc43
inflatus <inflatus@gmail.com>
2017-04-24 17:48:10 +0000

    minor change to code, trying to sort out bringing it together

--- scan_ip.py
+++ scan_ip.py
@@ -17,7 +17,7 @@ def get_domain_name(url):
     domain_name = get_tld(url)
     return domain_name
 print('Getting domain name')
-    
+
 # get ip address of top level domain
 def get_ip_address(url):
     command = 'host ' + url
@@ -26,7 +26,7 @@ def get_ip_address(url):
     marker = results.find('has address') + 12
     return results[marker:].splitlines()[0]
 print('Getting IP address')
-    
+
 # run nmap "fast" scan of ip address
 def get_nmap(ip):
     command = 'nmap -F ' + ip
@@ -42,15 +42,16 @@ create_directory(root_dir)
 def gather_info(name, url):
     domain_name = get_domain_name(url)
     ip_address = get_ip_address(url)
-    nmap = get_nmap('-F' + ip_address)
+    nmap = get_nmap(ip_address)
     create_report(name, url, domain_name, nmap)
-    
+
 def create_report(name, full_url, domain_name, nmap):
     project_dir = root_dir + '/' + name
     create_directory(project_dir)
     write_file(project_dir + '/full_url.txt', full_url)
     write_file(project_dir + '/ip_address.txt', get_ip_address)
     write_file(project_dir + '/nmap results.txt', nmap)
-    print('Finished gathering information from ', name)
+    
+print('Finished gathering information')
 
 gather_info('quantifiedcode', 'https://www.quantifiedcode.com/')
