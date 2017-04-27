9a2555929d80468ff6e4feaa7d2d042257403803
inflatus <inflatus@gmail.com>
2017-04-27 00:46:54 +0000

    changing flow and commands

--- ip_information_duo.py
+++ ip_information_duo.py
@@ -1,4 +1,18 @@
 import nmap
-nm = nmap.PortScannerYield()
-for progressive_result in nm.scan('127.0.0.1/24', '22-25'):
-    print(progressive_result)
+
+nm = nmap.PortScanner()
+nm.scan('127.0.0.1', '22-443')
+nm.command_line()
+
+for host in nm.all_hosts():
+    print('----------------------------------------------------')
+    print('Host : %s (%s)' % (host, nm[host].hostname()))
+    print('State : %s' % nm[host].state())
+    for proto in nm[host].all_protocols():
+        print('----------')
+        print('Protocol : %s' % proto)
+
+        lport = nm[host][proto].keys()
+        lport.sort()
+        for port in lport:
+            print ('port : %s\tstate : %s' % (port, nm[host][proto][port]['state']))
