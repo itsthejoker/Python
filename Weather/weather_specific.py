97b0b993226ba9f286c604d0e0cad69484307814
inflatus <inflatus@gmail.com>
2017-10-17 18:34:19 -0400

    rethinking weather script

---
+++ Weather/weather_specific.py
@@ -0,0 +1,12 @@
+# grab the current weather for Martinsville
+
+import requests
+r = requests.get('https://weather.com/weather/today/l/39.43,-86.42')
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(r.text, 'html.parser')
+results = soup.find('div', attrs={'class':'today_nowcard-temp'})
+
+temp = results.find('class')
+
+print(temp)
