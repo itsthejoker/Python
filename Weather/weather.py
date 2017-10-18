08056d7d354382aa1ef3c4a424db4d80cb507786
inflatus <inflatus@gmail.com>
2017-10-18 17:09:16 -0400

    added more output to script

--- Weather/weather.py
+++ Weather/weather.py
@@ -1,3 +1,5 @@
+# using json and the Weather Underground API to pull info
+
 import urllib.request
 import json
 from API_KEYS import WEATHER_UNDERGROUND_KEY
@@ -12,7 +14,23 @@ json_string = f.read().decode('utf-8')
 parsed_json = json.loads(json_string)
 location = parsed_json['location']['city']
 temp_f = parsed_json['current_observation']['temp_f']
+relative_humidity = parsed_json['current_observation']['relative_humidity']
+wind_mph = parsed_json['current_observation']['wind_mph']
+wind_gust = parsed_json['current_observation']['wind_gust_mph']
+pressure_mb = parsed_json['current_observation']['pressure_mb']
+feels_like = parsed_json['current_observation']['feelslike_f']
+visibility_mi = parsed_json['current_observation']['visibility_mi']
+precipitation_in = parsed_json['current_observation']['precip_today_in']
+weather = parsed_json['current_observation']['weather']
 
 # printing the location and temperature
-print('Current temperature in %s is: %s' % (location, temp_f))
+print('Current temperature in %s is: %s F' % (location, temp_f))
+print('Relative Humidity is at: %s' % (relative_humidity))
+print('Winds are: %s mph' % (wind_mph))
+print('Wind gusts are at: %s mph' % (wind_gust))
+print('Pressure is: %s mb' % (pressure_mb))
+print('Feels like: %s F' % (feels_like))
+print('Visisbility is: %s mi' % (visibility_mi))
+print('Precipitaion today: %s inches' % (precipitation_in))
+print('General weather is: %s' % (weather))
 f.close()
