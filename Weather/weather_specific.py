f687a529e12125b14434b8c81747da2eeb73acbd
inflatus <inflatus@gmail.com>
2017-10-18 10:32:33 -0400

    added more variables to output

--- Weather/weather_specific.py
+++ Weather/weather_specific.py
@@ -5,6 +5,14 @@ r = requests.get('https://weather.com/weather/today/l/39.43,-86.42')
 
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(r.text, 'html.parser')
-results = soup.find('div', attrs={'class':'today_nowcard-temp'})
 
-print('The temperature in Martinsville is',results.text)
+temp = soup.find('div', attrs={'class':'today_nowcard-temp'})
+feels_like = soup.find('div', attrs={'class':'today_nowcard-feels'})
+phrase = soup.find('div', attrs={'class':'today_nowcard-phrase'})
+hilo = soup.find('div', attrs={'class':'today_nowcard-hilo'})
+
+print('The temperature in Martinsville is',temp.text)
+print(feels_like.text)
+print(phrase.text)
+print(hilo.text[:13])
+print(hilo.text[13:])
