f1d0f6d482c2abcdf263298f2950912afd5b5127
inflatus <inflatus@gmail.com>
2017-10-23 19:36:49 -0400

    fixed loop issue

--- Scraping/apple_cve.py
+++ Scraping/apple_cve.py
@@ -17,16 +17,16 @@ parsed_json = json.loads(json_string)
 
 records = []
 num = 0
-while (num < 100):
-    cve = parsed_json[num]['id']
-    cwe = parsed_json[num]['cwe']
+if id != None:
+    cve = parsed_json[num].get('id', None)
+    cwe = parsed_json[num].get('cwe', None)
     summary = parsed_json[num]['summary']
     published = parsed_json[num]['Published']
-    last_modified = parsed_json[num]['last-modified']
+    last_modified = parsed_json[num].get('last-modified', None)
     records.append((cve, cwe, summary, published, last_modified))
     num = num + 1
-
+else:
 # export to csv
-df = pd.DataFrame(records, columns=('cve', 'cwe', 'summary', 'published', 'last_modified'))
-df.to_csv('cve_apple.csv', index=False, encoding='utf-8')
-f.close()
+    df = pd.DataFrame(records, columns=('cve', 'cwe', 'summary', 'published', 'last_modified'))
+    df.to_csv('cve_apple.csv', index=False, encoding='utf-8')
+    f.close()
