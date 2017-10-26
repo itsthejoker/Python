8a65f546e7a0b256cebe7467605e4cb2cffba999
inflatus <inflatus@gmail.com>
2017-10-26 19:53:52 -0400

    changed to pull 400 CVE

--- Scraping/apple_cve.py
+++ Scraping/apple_cve.py
@@ -1,6 +1,6 @@
 # Apple's current CVE list from
 # circl.lu
-# pulling all the CVE's
+# pulling 400 of the CVE's
 
 import urllib.request
 import json
@@ -17,16 +17,16 @@ parsed_json = json.loads(json_string)
 
 records = []
 num = 0
-if id != None:
+while (num < 400):
     cve = parsed_json[num].get('id', None)
     cwe = parsed_json[num].get('cwe', None)
-    summary = parsed_json[num]['summary']
-    published = parsed_json[num]['Published']
+    summary = parsed_json[num].get('summary', None)
+    published = parsed_json[num].get('Published', None)
     last_modified = parsed_json[num].get('last-modified', None)
     records.append((cve, cwe, summary, published, last_modified))
     num = num + 1
-else:
-# export to csv
-    df = pd.DataFrame(records, columns=('cve', 'cwe', 'summary', 'published', 'last_modified'))
-    df.to_csv('cve_apple.csv', index=False, encoding='utf-8')
-    f.close()
+
+#export to csv
+df = pd.DataFrame(records, columns=('cve', 'cwe', 'summary', 'published', 'last_modified'))
+df.to_csv('cve_apple.csv', index=False, encoding='utf-8')
+f.close()
